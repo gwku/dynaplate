24f1078d586b8cb7ecdd5eea680c62b10166ea14
Gerwin Kuijntjes <administratie@gerwinkuijntjes.nl>
2024-11-07 17:57:39 +0100

    Renamed autoplate to dynaplate

--- src/cli/args.rs
+++ src/cli/args.rs
@@ -2,7 +2,7 @@ use clap::Parser;
 use std::path::PathBuf;
 
 #[derive(Parser)]
-#[command(name = "Autoplate")]
+#[command(name = "Dynaplate")]
 #[command(about = "A tool to automate project templates", version = "1.0")]
 pub struct Args {
     #[clap(required = true)]
