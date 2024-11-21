0a1145ca56f365f5babb4b5dfbae97897b95a2ca
Gerwin Kuijntjes <administratie@gerwinkuijntjes.nl>
2024-11-21 13:03:37 +0100

    Removed unused method extract_arguments

--- src/utils/command.rs
+++ src/utils/command.rs
@@ -55,7 +55,7 @@ pub fn execute_command<T: CommandTrait + ConditionTrait>(
 
     let cmd_with_variables_replaced =
         replace_variables(command.command(), &project.variables, &project.clean)?;
-    
+
     let mut cmd = std::process::Command::new("sh");
     cmd.arg("-c").arg(cmd_with_variables_replaced);
 
@@ -77,39 +77,4 @@ pub fn execute_command<T: CommandTrait + ConditionTrait>(
             source: e,
         }),
     }
-}
-
-fn extract_arguments(cmd_with_variables_replaced: String) -> Result<Vec<String>, UtilsResult<()>> {
-    let mut args = Vec::new();
-    let mut current_arg = String::new();
-    let mut in_quotes = false;
-
-    let chars: Vec<char> = cmd_with_variables_replaced.chars().collect();
-
-    for c in chars.iter() {
-        match *c {
-            '"' | '\'' => {
-                // Toggle the quoted state
-                in_quotes = !in_quotes;
-            }
-            ' ' if !in_quotes => {
-                // End of an argument, if not inside quotes
-                if !current_arg.is_empty() {
-                    args.push(current_arg.clone());
-                    current_arg.clear();
-                }
-            }
-            _ => {
-                // Collect characters into the current argument
-                current_arg.push(*c);
-            }
-        }
-    }
-
-    // Push the last argument if any
-    if !current_arg.is_empty() {
-        args.push(current_arg);
-    }
-
-    Ok(args)
 }
